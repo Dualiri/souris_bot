af3f820f6249bc82c87ab04b72a881190abacf36
José Lucas de Souza Andrade <4870354-lucasc3x@users.noreply.gitlab.com>
2019-12-21 13:31:28 -0300

    Fix heroku

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js")
+// const Discord = require("discord.js")
 const { Client } = require("discord.js")
 //const { command } = require("discord.js-comando")
 
@@ -6,30 +6,36 @@ const client = new Client({
   disableEveryone: true
 })
 
+client.login("NjU2NjUzOTA3NzAyOTcyNDM4.Xf5H0w.u0sPTAalHxkwxvx97hq8kY6OCzA")
+	.then(() => {
+		console.log('Success')
+	})
+	.catch((err) => {
+		console.log(err)
+	})
+
 client.on("ready", () => {
   console.log("I'm online bb")
 })
 
 client.on("message", async message => {
-        if(message.author.bot) return
-        if(message.channel.type === "dm") return
+	if(message.author.bot) return
+	if(message.channel.type === "dm") return
 
-        //let command = message.content[0]
-        //console.log(command)
-        let command = message.content.toLowerCase()
+	//let command = message.content[0]
+	//console.log(command)
+	let command = message.content.toLowerCase()
 
-        //if (command !== "/") return
+	//if (command !== "/") return
 
-        if (command === "queijo") {
-                message.channel.send("Dou 10 conto nele")
-        } else if (command === 'shanub') {
-                message.channel.send("|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|—|¯¯¯¯¯\\ \n| SHANUB ENCOMENDAS |» ¦  []  []  |\n|=¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯=)   ]  ------  ]\n¯¯(@@)¯¯¯(@@)¯¯¯¯¯¯¯¯¯¯¯¯¯(@)")
-        } else if (command === 'firstei') {
+	if (command === "queijo") {
+		message.channel.send("Dou 10 conto nele")
+	} else if (command === 'shanub') {
+		message.channel.send("|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|—|¯¯¯¯¯\\ \n| SHANUB ENCOMENDAS |» ¦  []  []  |\n|=¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯=)   ]  ------  ]\n¯¯(@@)¯¯¯(@@)¯¯¯¯¯¯¯¯¯¯¯¯¯(@)")
+	} else if (command === 'firstei') {
 		message.channel.send("First nub")
 	}
 
 
-        console.log(`${message.author.username} said: ${message.content}`)
+	console.log(`${message.author.username} said: ${message.content}`)
 })
-
-client.login("NjU2NjUzOTA3NzAyOTcyNDM4.Xfrlxw.c5ShifStInMnThMZ0TGFcYWL9ss").then(() => {}).catch(() => {})
